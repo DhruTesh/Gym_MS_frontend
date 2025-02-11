38a7e64cdb3c22fd7f4e88e3c520935fdc864c8d
DhruTesh <dhruteshdev5@gmail.com>
2025-02-11 18:16:53 +0530

    Forgot Password

---
+++ src/Components/ForgotPassword/forgotPassword.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react'
+
+const ForgotPassword = () => {
+    const [emailSubmit, setEmailSubmit] = useState(false)
+    const [otpValidate,setOtpValidate] = useState(false)
+    const [contentVal,setContentValue] = useState("Submit Your Email")
+
+    const handleSubmit= () => {
+        if (!emailSubmit) {
+            setEmailSubmit(true)
+            setContentValue("Submit OTP")
+        }else if (emailSubmit && !otpValidate) {
+            setOtpValidate(true)
+            setContentValue("Submit New Password")
+        }
+            
+        }
+    
+
+    return (
+        <div className='w-full'>
+            <div className='w-full mb-5'>
+                <div>Enter your Email</div>
+                <input type='text' className='w-1/2  p-2 rounded-lg border-2 text-black ' placeholder='Enter Email' />
+            </div>
+
+            {
+                emailSubmit && <div className='w-full mb-5'>
+                    <div>Enter your OTP</div>
+                    <input type='text' className='w-1/2  p-2 rounded-lg border-2 text-black ' placeholder='Enter OTP' />
+                </div>
+            }
+
+{
+                otpValidate && <div className='w-full mb-5'>
+                    <div>Enter your New Password</div>
+                    <input type='text' className='w-1/2  p-2 rounded-lg border-2 text-black ' placeholder='Enter New Password' />
+                </div>
+            }
+
+            <div className='bg-slate-800 text-white mx-auto w-2/3 p-3 rounded-lg text-center text-lg border-2 hover:bg-white hover:text-black font-semibold cursor-pointer' onClick={() => handleSubmit()}>{contentVal}</div>
+
+        </div>
+    )
+}
+
+export default ForgotPassword 
--- src/Components/Signup/signUp.jsx
+++ src/Components/Signup/signUp.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './signUp.css'
+import Modal from '../Modal/Modal'
+import ForgotPassword from '../ForgotPassword/forgotPassword';
+
+
+
 
 const SignUp = () => {
+
+    const [forgotPassword, setForgotPassword] = useState(false);
+
+    const handleClose = () => {
+        setForgotPassword(prev => !prev);
+    }
+
+    
     return (
         <div className='customSignup w-1/3 p-10 mt-20 ml-20 bg-gray-500 opacity-50 h-[500px] overflow-y-auto'>
             <div className='font-sans  text-white text-center text-3xl'>Register the GYM </div>
@@ -13,7 +26,8 @@ const SignUp = () => {
             <img src='https://images.unsplash.com/photo-1577221084712-45b0445d2b00?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8dGhlJTIwZ3ltfGVufDB8fDB8fHww' className='mb-10 h-[200px] w-[250px]' />
 
             <div className='p-2 w-[80%] border-2 bg-slate-800 mx-auto rounded-lg text-white text-center text-lg hover:bg-white hover:text-black font-semibold cursor-pointer'>Register</div>
-            <div className='p-2 w-[80%] mt-5 border-2 bg-slate-800 mx-auto rounded-lg text-white text-center text-lg hover:bg-white hover:text-black font-semibold cursor-pointer'>Forgot Password</div>
+            <div className='p-2 w-[80%] mt-5 border-2 bg-slate-800 mx-auto rounded-lg text-white text-center text-lg hover:bg-white hover:text-black font-semibold cursor-pointer ' onClick={() => handleClose()}>Forgot Password</div>
+            {forgotPassword && <Modal header='Forgot Password' handleClose={handleClose}  content={<ForgotPassword />} />}
         </div>
 
     )
